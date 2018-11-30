7de9918e91b1817591f208db681567cbe86d5dc7
AM_Myrick <amyrick.820@gmail.com>
2018-11-30 11:51:02 -0500

    completes server

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
+const bcrypt = require("bcryptjs");
+const knex = require("knex");
+const knexConfig = require("../knexfile");
+const db = knex(knexConfig.development);
 
-const { authenticate } = require('./middlewares');
+const { authenticate, generateToken } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,17 +13,54 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  const creds = req.body;
+
+  if (creds.username === "" || creds.username === undefined) {
+    return res.status(400).json({error: "Please enter a username."})
+  }
+
+  if (creds.password === "" || creds.password === undefined) {
+    return res.status(400).json({error: "Please enter a password."})
+  }
+
+  const hash = bcrypt.hashSync(creds.password, 8);
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => res.status(201).json({message: "welcome", ids}))
+    .catch(err => res.status(401).json(err))
 }
 
 function login(req, res) {
-  // implement user login
+  const creds = req.body;
+
+  if (creds.username === "" || creds.username === undefined) {
+      return res.status(400).json({error: "Please enter a username."})
+  }
+
+  if (creds.password === "" || creds.password === undefined) {
+      return res.status(400).json({error: "Please enter a password."})
+  }
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: 'welcome!', token });
+      } else {
+        res.status(401).json({ message: 'you shall not pass!!' });
+      }
+    })
+    .catch(err => res.json(err));
 }
 
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      'https://safe-falls-22549.herokuapp.com/random_ten'
     )
     .then(response => {
       res.status(200).json(response.data);
